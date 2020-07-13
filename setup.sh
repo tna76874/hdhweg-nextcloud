727f174c9aaebfc047054543dc9d7f4f9d76798a
lmh <lmh@local.de>
2020-07-13 18:50:56 +0200

    dont supress apt install in setup script

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ rm -rf ${REPODIR}
 
 echo -e "... update system sources, install ansible and git and clone the playbook repository ... [this could take a few minutes now, depending on your internet connection]"
 sudo apt update > /dev/null 2>&1
-sudo apt install software-properties-common -y > /dev/null 2>&1
+sudo apt install software-properties-common -y
 sudo apt-add-repository universe > /dev/null 2>&1
 sudo apt-add-repository multiverse > /dev/null 2>&1
 if [ "$DISTRIB_CODENAME" = "bionic" ]
@@ -18,7 +18,7 @@ then
     sudo apt-add-repository --yes --update ppa:ansible/ansible > /dev/null 2>&1
 fi
 sudo apt update > /dev/null 2>&1
-sudo apt install nano git ansible -y > /dev/null 2>&1
+sudo apt install nano git ansible -y
 
 #clone the playbook repository
 git clone https://github.com/tna76874/hdhweg-nextcloud.git ${REPODIR} > /dev/null 2>&1
